ecc218e4ca5d187b823e81612549bdfb78043464
Jake Smith <Jake.Smith@synergy-logistics.co.uk>
2023-10-16 13:38:42 +0100

    adding finished lab3

--- ExamAverage.py
+++ ExamAverage.py
@@ -1,13 +1,18 @@
-math = int(input("Enter maths result"))
+math = int(input("Enter maths result: "))
 while math <= 0 or math >= 100:
     print("Please enter valid result")
-english = int(input("Enter English result"))
+english = int(input("Enter English result: "))
 while english <= 0 or english >= 100:
     print("Please enter valid result")
-ICT = int(input("Enter ICT result"))
+ICT = int(input("Enter ICT result: "))
 while ICT <= 0 or ICT >= 100:
     print("Please enter valid result")
 
 average = (math + english + ICT)/3
 
-print(average)
+if average < 65:
+    print(f"You have failed, your average score was {average}")
+
+else:
+    print(f"You have passed, your average score was {average}")
+
--- squares.py
+++ squares.py
@@ -2,6 +2,6 @@ a = 0
 while a <= 100:
     result = a*a
     print(result)
-    if result >= 2000:
+    if result > 2000:
         break
     a += 1
